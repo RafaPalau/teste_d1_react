e5ed07bdbd115dfdedc01b5098d296a9fc9cf6b9
RafaPalau <rafaeldonizetip@gmail.com>
2020-08-27 22:22:48 -0300

    Novo build

--- src/setupTest.js
+++
@@ -1,6 +0,0 @@
-// Importing the enzyme and the Adapter.
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-// Setting up the Enzyme test environment.
-configure({ adapter: new Adapter() });
